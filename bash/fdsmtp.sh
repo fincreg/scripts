8d9eb957dd6c3021734dc2528dba9406d069877b
fincreg <fincreg@users.noreply.github.com>
2021-11-23 13:31:06 +0200

    rcpt and subject customization

--- bash/fdsmtp.sh
+++ bash/fdsmtp.sh
@@ -8,39 +8,45 @@ host=$2
 port=25
 user=$3
 pass=$4
-from=$user
-to=$user
-step=0
-data="Subject: SMTP Check
-From: $from
-To: $to
+from=${user}
+rcpt=${5:-${user}}
+subj=${6:-"TEST Message"}
 
-Hello!
-test
+data="X-Mailer: BASH
+Subject: $subj
+From: \"$from\" <$from>
+To: <$rcpt>
+
+Delivarability test message
+
+Sincerely,
+BASH Script
+
 "
 
 smtp () {
   exec 10<>/dev/tcp/$host/$port
   exec 4> >(tee >(cat - >&10))
 
+  step=0
   while read -u10 r
   do
     echo "$r"
     case "$r:$auth:$step" in
-      "220 "*)                    echo -ne "EHLO test\r\n" >&4                                         ;;
-      "250 "[A-Za-z]*:LOGIN:0)    echo -ne "AUTH LOGIN\r\n" >&4         ; let step++                   ;;
-      "250 "[A-Za-z]*:PLAIN:0)    echo -ne "AUTH PLAIN\r\n" >&4         ; let step++                   ;;
-      "250 "[A-Za-z]*:CRAM-MD5:0) echo -ne "AUTH CRAM-MD5\r\n" >&4      ; let step++                   ;;
+      "220 "*)                      echo -ne "EHLO ${from#*@}\r\n" >&4                                 ;;
+      "250 "[A-Za-z]*:LOGIN:0)      echo -ne "AUTH LOGIN\r\n" >&4                      ; let step++    ;;
+      "250 "[A-Za-z]*:PLAIN:0)      echo -ne "AUTH PLAIN\r\n" >&4                      ; let step++    ;;
+      "250 "[A-Za-z]*:CRAM-MD5:0)   echo -ne "AUTH CRAM-MD5\r\n" >&4                   ; let step++    ;;
       "334 VXNlcm5hbWU6"*:LOGIN:1)  echo -ne "$(b64 $user)\r\n" >&4                                    ;;
-      "334 UGFzc3dvcmQ6"*:LOGIN:1)  echo -ne "$(b64 $pass)\r\n" >&4     ; let step++                   ;;
+      "334 UGFzc3dvcmQ6"*:LOGIN:1)  echo -ne "$(b64 $pass)\r\n" >&4                    ; let step++    ;;
       "334"*:PLAIN:1)               echo -ne "$(b64 "\0$user\000$pass")\r\n" >&4       ; let step++    ;;
       "334"*:CRAM-MD5:1)            echo -ne "$(cram-md5 $user $pass ${r/* })\r\n" >&4 ; let step++    ;;
-      "235 "*:*:2)                    echo -ne "MAIL FROM: $from\r\n" >&4              ; let step++    ;;
-      "250 "*:*:3)                echo -ne "RCPT TO: $to\r\n" >&4       ; let step++                   ;;
-      "250 "*:*:4)                echo -ne "DATA\r\n$data\r\n.\r\n" >&4 ; let step++                   ;;
-      "250 "*:*:5)                echo -ne "QUIT\r\n" >&4               ; let step++                   ;;
-      "250-"*) ;; "221 "*) ;; 
-      *)                          echo -ne "QUIT\r\n" >&4                                              ;;
+      "235 "*:*:2)                  echo -ne "MAIL FROM: <$from>\r\n" >&4              ; let step++    ;;
+      "250 2.1.0"*:*:3)             echo -ne "RCPT TO: <$rcpt>\r\n" >&4                ; let step++    ;;
+      "250 2.1.5"*:*:4)             echo -ne "DATA\r\n$data\r\n.\r\n" >&4              ; let step++    ;;
+      "250 2.0.0"*:*:5)             echo -ne "QUIT\r\n" >&4                            ; let step++    ;;
+      "250-"*)                                                                                         ;;
+      "221 "*)                                                                                         ;;
     esac
   done
 
