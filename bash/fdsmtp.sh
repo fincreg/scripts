6c9ed23112d304388de8fdfac60db5dc49d84ebb
fincreg <fincreg@users.noreply.github.com>
2026-05-05 20:35:19 +0300

    Refactor fdsmtp.sh
    - Add LOGINONLY for auth check without sending
    - Add timeout 10s to kill hungs
    - Return port argument
    - Correct 10 year typo

--- bash/fdsmtp.sh
+++ bash/fdsmtp.sh
@@ -5,19 +5,19 @@
 
 auth=$1
 host=$2
-port=25
-user=$3
-pass=$4
+port=$3
+user=$4
+pass=$5
 from=${user}
-rcpt=${5:-${user}}
-subj=${6:-"TEST Message"}
+rcpt=${6:-${user}}
+subj=${7:-"TEST Message"}
 
 data="X-Mailer: BASH
 Subject: $subj
 From: \"$from\" <$from>
 To: <$rcpt>
 
-Delivarability test message
+Deliverability test message
 
 Sincerely,
 BASH Script
@@ -25,36 +25,44 @@ BASH Script
 "
 
 smtp () {
-  exec 10<>/dev/tcp/$host/$port
+  exec 10<>/dev/tcp/$host/$port || return 1
   exec 4> >(tee >(cat - >&10))
 
+  if [[ "$auth" == "LOGINONLY" ]]; then
+    auth="LOGIN"
+    nosending=true
+  fi
+
   step=0
-  while read -u10 r
+  while read -u10 -t 10 r
   do
     echo "$r"
     case "$r:$auth:$step" in
-      "220 "*)                      echo -ne "EHLO ${from#*@}\r\n" >&4                                 ;;
-      "250 "[A-Za-z]*:NOLOGIN:0)    echo -ne "MAIL FROM: <$from>\r\n" >&4              ; let step=3    ;;
-      "250 "[A-Za-z]*:LOGIN:0)      echo -ne "AUTH LOGIN\r\n" >&4                      ; let step++    ;;
-      "250 "[A-Za-z]*:PLAIN:0)      echo -ne "AUTH PLAIN\r\n" >&4                      ; let step++    ;;
-      "250 "[A-Za-z]*:CRAM-MD5:0)   echo -ne "AUTH CRAM-MD5\r\n" >&4                   ; let step++    ;;
-      "334 VXNlcm5hbWU6"*:LOGIN:1)  echo -ne "$(b64 $user)\r\n" >&4                                    ;;
-      "334 UGFzc3dvcmQ6"*:LOGIN:1)  echo -ne "$(b64 $pass)\r\n" >&4                    ; let step++    ;;
-      "334"*:PLAIN:1)               echo -ne "$(b64 "\0$user\000$pass")\r\n" >&4       ; let step++    ;;
-      "334"*:CRAM-MD5:1)            echo -ne "$(cram-md5 $user $pass ${r/* })\r\n" >&4 ; let step++    ;;
-      "235 "*:*:2)                  echo -ne "MAIL FROM: <$from>\r\n" >&4              ; let step++    ;;
-      "250 2.1.0"*:*:3)             echo -ne "RCPT TO: <$rcpt>\r\n" >&4                ; let step++    ;;
-      "250 2.1.5"*:*:4)             echo -ne "DATA\r\n$data\r\n.\r\n" >&4              ; let step++    ;;
-      "250 2.0.0"*:*:5)             echo -ne "QUIT\r\n" >&4                            ; let step++    ;;
-      "250-"*)                                                                                         ;;
-      "221 "*)                                                                                         ;;
+      "220 "*)                      echo -ne "EHLO ${from#*@}\r\n" >&4                              ;;
+      "250 "[A-Za-z]*:NOLOGIN:0)    echo -ne "MAIL FROM: <$from>\r\n" >&4              ; ((step=3)) ;;
+      "250 "[A-Za-z]*:LOGIN:0)      echo -ne "AUTH LOGIN\r\n" >&4                      ; ((step++)) ;;
+      "250 "[A-Za-z]*:PLAIN:0)      echo -ne "AUTH PLAIN\r\n" >&4                      ; ((step++)) ;;
+      "250 "[A-Za-z]*:CRAM-MD5:0)   echo -ne "AUTH CRAM-MD5\r\n" >&4                   ; ((step++)) ;;
+      "334 VXNlcm5hbWU6"*:LOGIN:1)  echo -ne "$(b64 $user)\r\n" >&4                                 ;;
+      "334 UGFzc3dvcmQ6"*:LOGIN:1)  echo -ne "$(b64 $pass)\r\n" >&4;\
+                                    [[ "$nosending" == true ]] && ((step=9)) ||          ((step++)) ;;
+      "334"*:PLAIN:1)               echo -ne "$(b64 "\0$user\000$pass")\r\n" >&4       ; ((step++)) ;;
+      "334"*:CRAM-MD5:1)            echo -ne "$(cram-md5 $user $pass ${r/* })\r\n" >&4 ; ((step++)) ;;
+      "235 "*:*:2)                  echo -ne "MAIL FROM: <$from>\r\n" >&4              ; ((step++)) ;;
+      "250 2.1.0"*:*:3)             echo -ne "RCPT TO: <$rcpt>\r\n" >&4                ; ((step++)) ;;
+      "250 2.1.5"*:*:4)             echo -ne "DATA\r\n" >&4; sleep .5;\
+                                    echo -ne "$data\r\n.\r\n" >&4                      ; ((step++)) ;;
+      "250 2.0.0"*:*:5)             echo -ne "QUIT\r\n" >&4                            ; ((step++)) ;;
+      "235 "*:*:9)                  echo -ne "QUIT\r\n" >&4                            ; ((step++)) ;;
+      "250-"*)                                                                                      ;;
+      "221 "*)                                                                                      ;;
     esac
   done
 
   exec 10<&-
   exec 4<&-
 
-  return $true
+  return 0
 }
 
 function cram-md5 () { 
